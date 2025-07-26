a940b7e7f46296111cf9174205839218932f7496
Sudarshan P <107467585+sudarshan441@users.noreply.github.com>
2025-07-26 22:55:37 +0530

    changed an anchor to Link tag in not-found.js

--- client/app/not-found.js
+++ client/app/not-found.js
@@ -1,9 +1,14 @@
+'use client';
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
-    <div className="text-center mt-20 space-y-4">
-      <h1 className="text-3xl font-bold text-red-600">404 - Page Not Found</h1>
-      <p className="text-gray-600">Sorry, we couldn't find what you're looking for.</p>
-      <a href="/" className="text-blue-600 underline">Go back home</a>
+    <div className="text-center mt-20">
+      <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="mb-4">Sorry, we can&apos;t find that page.</p>
+      <Link href="/" className="text-blue-600 underline">
+        Go back home
+      </Link>
     </div>
   );
 }
